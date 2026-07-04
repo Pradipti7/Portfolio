2d711ced74d88e6504bcae421c74b7ada27248c5
Pradipti7 <pradiptipokharel7@gmail.com>
2026-07-04 06:17:21 +0545

    add:footer added

---
+++ src/assets/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-[#2C2416] text-white py-10">
+      <div className="text-center">
+        <p>© 2026 Pradipti Pokharel</p>
+      </div>
+    </footer>
+  );
+}
